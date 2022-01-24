57f1ab735670d1644caf1c20a2d64a06190141b7
Silas Boyd-Wickizer <silasbw@gmail.com>
2022-01-24 10:35:48 -0800

    Updated

--- scripts/resolve.ts
+++ scripts/resolve.ts
@@ -1,17 +1,15 @@
 /* eslint no-console: "off" */
 import { newKit } from '@celo/contractkit'
-import { NomKit } from '@nomspace/nomspace'
 import yargs from 'yargs'
 
 import { ResolveGroup, ResolveAddress, ResolveNom } from '../src'
 
 async function main(args: any) {
   const kit = newKit('https://forno.celo.org')
-  const nomKit = new NomKit(kit, ResolveNom.MainnetContractAddress)
 
   const resolver = new ResolveGroup([
     new ResolveAddress(),
-    new ResolveNom(nomKit),
+    new ResolveNom({ kit, contractAddress: ResolveNom.MainnetContractAddress }),
   ])
   const resolutions = await resolver.resolve(args.id)
 
