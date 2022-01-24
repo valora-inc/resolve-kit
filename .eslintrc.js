deb302369971e31b3f8d2cd2a27f4facb596d302
Silas Boyd-Wickizer <silasbw@gmail.com>
2022-01-23 17:29:44 -0800

    chore: add a cli as an example / exploring from the command line

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['@valora/eslint-config-typescript', 'plugin:json/recommended'],
   parserOptions: {
-    project: './tsconfig.json',
+    project: './tsconfig.eslint.json',
   }
 }
