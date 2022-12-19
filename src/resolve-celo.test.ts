c5ab9f8d120757ad1ff26615c7c5c16cd182ec1e
Silas Boyd-Wickizer <silasbw@gmail.com>
2022-12-19 08:15:16 -0800

    feat: add an asv2 resolver

---
+++ src/resolve-celo.test.ts
@@ -0,0 +1,141 @@
+import { ResolveCelo } from './resolve-celo'
+import { AuthSigner, ServiceContext } from '@celo/identity/lib/odis/query'
+import Web3 from 'web3'
+import * as Identity from '@celo/identity'
+
+jest.mock('web3')
+jest.mock('@celo/identity')
+
+describe('resolve-celo', () => {
+  describe('ResolveCelo', () => {
+    beforeEach(() => {
+      jest.resetAllMocks()
+    })
+
+    it('returns the expected resolutions', async () => {
+      const Web3Mock: any = jest.mocked(Web3)
+      Web3Mock.mockImplementation(() => {
+        return {
+          eth: {
+            Contract: jest.fn().mockImplementation(() => ({
+              methods: {
+                lookupAttestations: () => {
+                  return {
+                    call: () => ({
+                      accounts: ['0xf00ba7'],
+                    }),
+                  }
+                },
+              },
+            })),
+          },
+        }
+      })
+
+      const identityMock: any = jest.mocked(Identity)
+      identityMock.OdisUtils = {
+        Identifier: {
+          getObfuscatedIdentifier: () => ({ obfuscatedIdentifier: '' }),
+          IdentifierPrefix: {},
+        },
+      }
+
+      const authSigner = {} as any as AuthSigner
+      const serviceContext = {} as any as ServiceContext
+
+      const resolver = new ResolveCelo({
+        providerUrl: 'http://does.not.matter',
+        federatedAttestationsProxyContractAddress: '0x1',
+        authSigner,
+        account: '0x3',
+        serviceContext,
+      })
+
+      const resolutions = await resolver.resolve('+18888888888')
+      expect(resolutions.resolutions.length).toBe(1)
+      expect(resolutions.errors.length).toBe(0)
+      const resolution = resolutions.resolutions[0]
+      expect(resolution.address).toBe('0xf00ba7')
+    })
+
+    it('returns errors on contract call error', async () => {
+      const Web3Mock: any = jest.mocked(Web3)
+      Web3Mock.mockImplementation(() => {
+        return {
+          eth: {
+            Contract: jest.fn().mockImplementation(() => ({
+              methods: {
+                lookupAttestations: () => {
+                  return {
+                    call: () => {
+                      throw new Error('foo')
+                    },
+                  }
+                },
+              },
+            })),
+          },
+        }
+      })
+
+      const identityMock: any = jest.mocked(Identity)
+      identityMock.OdisUtils = {
+        Identifier: {
+          getObfuscatedIdentifier: () => ({ obfuscatedIdentifier: '' }),
+          IdentifierPrefix: {},
+        },
+      }
+
+      const authSigner = {} as any as AuthSigner
+      const serviceContext = {} as any as ServiceContext
+
+      const resolver = new ResolveCelo({
+        providerUrl: 'http://does.not.matter',
+        federatedAttestationsProxyContractAddress: '0x1',
+        authSigner,
+        account: '0x3',
+        serviceContext,
+      })
+
+      const resolutions = await resolver.resolve('+18888888888')
+      expect(resolutions.resolutions.length).toBe(0)
+      expect(resolutions.errors.length).toBe(1)
+    })
+
+    it('returns errors on ODIS SDK error', async () => {
+      const Web3Mock: any = jest.mocked(Web3)
+      Web3Mock.mockImplementation(() => {
+        return {
+          eth: {
+            Contract: jest.fn().mockImplementation(),
+          },
+        }
+      })
+
+      const identityMock: any = jest.mocked(Identity)
+      identityMock.OdisUtils = {
+        Identifier: {
+          getObfuscatedIdentifier: () => {
+            throw new Error('foo')
+          },
+          IdentifierPrefix: {},
+        },
+      }
+
+      const authSigner = {} as any as AuthSigner
+      const serviceContext = {} as any as ServiceContext
+
+      const resolver = new ResolveCelo({
+        providerUrl: 'http://does.not.matter',
+        federatedAttestationsProxyContractAddress: '0x1',
+        authSigner,
+        account: '0x3',
+        serviceContext,
+      })
+
+      const resolutions = await resolver.resolve('+18888888888')
+      expect(resolutions.resolutions.length).toBe(0)
+      expect(resolutions.errors.length).toBe(1)
+    })
+  })
+})
---
+++ src/resolve-celo.ts
@@ -0,0 +1,88 @@
+import Web3 from 'web3'
+import { Address } from '@celo/base/lib/address'
+import { AbiItem } from 'web3-utils'
+import { Contract } from 'web3-eth-contract'
+import { OdisUtils } from '@celo/identity'
+import { AuthSigner, ServiceContext } from '@celo/identity/lib/odis/query'
+
+import { NameResolver, NameResolutionResults, ResolutionKind } from './types'
+
+const DEFAULT_NETWORK_TIMEOUT = 5_000
+
+export class ResolveCelo implements NameResolver {
+  private federatedAttestationsContract: Contract
+  private authSigner: AuthSigner
+  private account: Address
+  private serviceContext: ServiceContext
+
+  static readonly FederatedAttestationsContractAbi: AbiItem[] =
+    require('./abis/FederatedAttestations.json').abi
+
+  // https://github.com/celo-org/identity/blob/ASv2/asv2/protocol.md#smart-contract-addresses
+  static readonly AlfajoresFederatedAttestationsProxyContractAddress: Address =
+    '0x70F9314aF173c246669cFb0EEe79F9Cfd9C34ee3'
+  static readonly MainnetFederatedAttestationsProxyContractAddress: Address =
+    '0x0aD5b1d0C25ecF6266Dd951403723B2687d6aff2'
+
+  constructor({
+    providerUrl,
+    federatedAttestationsProxyContractAddress,
+    authSigner,
+    account,
+    serviceContext,
+    networkTimeout,
+  }: {
+    providerUrl: string
+    federatedAttestationsProxyContractAddress: Address
+    authSigner: AuthSigner
+    account: Address
+    serviceContext: ServiceContext
+    networkTimeout?: number
+  }) {
+    networkTimeout = networkTimeout ?? DEFAULT_NETWORK_TIMEOUT
+    const web3 = new Web3(
+      new Web3.providers.HttpProvider(providerUrl, { timeout: networkTimeout }),
+    )
+    const federatedAttestationsContract = new web3.eth.Contract(
+      ResolveCelo.FederatedAttestationsContractAbi,
+      federatedAttestationsProxyContractAddress,
+    )
+
+    this.federatedAttestationsContract = federatedAttestationsContract
+    this.authSigner = authSigner
+    this.account = account
+    this.serviceContext = serviceContext
+  }
+
+  async resolve(id: string): Promise<NameResolutionResults> {
+    try {
+      // TODO(sbw): need a timeout
+      const identifier = (
+        await OdisUtils.Identifier.getObfuscatedIdentifier(
+          id,
+          OdisUtils.Identifier.IdentifierPrefix.PHONE_NUMBER,
+          this.account,
+          this.authSigner,
+          this.serviceContext,
+        )
+      ).obfuscatedIdentifier
+
+      const attestations = await this.federatedAttestationsContract.methods
+        .lookupAttestations(identifier, [this.account])
+        .call()
+
+      return {
+        resolutions: attestations.accounts.map((address: Address) => ({
+          kind: ResolutionKind.Celo,
+          address,
+        })),
+        errors: [],
+      }
+    } catch (error) {
+      return {
+        resolutions: [],
+        errors: [{ kind: ResolutionKind.Celo, error: error as Error }],
+      }
+    }
+  }
+}
